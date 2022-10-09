89930b5840e3522d3da4679515ec22e659241238
xProioxis <christionb@hotmail.com>
2022-10-09 07:43:34 -0500

    Update submission.py

--- Challenges/TamuDatathon/TamuDatathon SourceCode/src/submission.py
+++ Challenges/TamuDatathon/TamuDatathon SourceCode/src/submission.py
@@ -3,6 +3,7 @@
 # This file should stay named as `submission.py`
 
 # Import Python Libraries
+from xmlrpc.client import MAXINT
 import numpy as np
 from glob import glob
 from PIL import Image
@@ -24,7 +25,7 @@ class Predictor:
         """
         Initializes any variables to be used when making predictions
         """
-        self.model = load_model('example_model.h5')
+        self.model = load_model('seq_model3.h5')
 
     def make_prediction(self, img_path):
         """
@@ -52,16 +53,69 @@ class Predictor:
         img_tensor = np.expand_dims(img_array, axis=0)
 
         # Preform a prediction on this image using a pre-trained model (you should make your own model :))
-        self.model.verbose = 0
-        prediction = self.model.predict(img_tensor, verbose=False)
+        #prediction = self.model.predict(img_tensor, verbose=False)
 
+
+        #fullImage = stitch(pieces)
+        #stitch
+
+
+        example_image = Image.open(img_path)
+        pieces = utils.get_uniform_rectangular_split(np.asarray(example_image), 2, 2)
+
+
+        temp1,temp2 = pictureCombos(pieces)
+
+    
+
+        min = MAXINT
+        ind = -1
+
+        for index, i in enumerate(temp1):
+            # Load the image
+
+            # Converts the image to a 3D numpy array (128x128x3)
+            img_array = img_to_array(i)
+
+            # Convert from (128x128x3) to (Nonex128x128x3), for tensorflow
+            img_tensor = np.expand_dims(img_array, axis=0)
+            self.model.verbose = 0
+            temp = self.model.predict(img_tensor)
+
+            if temp < min:
+                min = temp
+                ind = index
+
+        
+        return str(temp2[ind][0]) + str(temp2[ind][1]) + str(temp2[ind][2]) + str(temp2[ind][3]) 
+
+
+
+        
         # The example model was trained to return the percent chance that the input image is scrambled using 
         # each one of the 24 possible permutations for a 2x2 puzzle
-        combs = [''.join(str(x) for x in comb) for comb in list(permutations(range(0, 4)))]
+        #combs = [''.join(str(x) for x in comb) for comb in list(permutations(range(0, 4)))]
 
         # Return the combination that the example model thinks is the solution to this puzzle
         # Example return value: `3120`
-        return combs[np.argmax(prediction)]
+        return #combs[np.argmax(prediction)]
+
+def pictureCombos(pieces):
+    key = [3,1,2,0] # example key
+
+    #fullImage = stitch(pieces)
+    #stitch
+    images = []
+    perms = []
+
+    for index, num in enumerate(permutations([0,1,2,3])):
+        final_image = Image.fromarray(np.vstack((np.hstack((pieces[num[0]],pieces[num[1]])),np.hstack((pieces[num[2]],pieces[num[3]])))))
+        #final_image.save(str(index) + "test.png")
+        images.append(final_image)
+        perms.append(num)
+    return np.array(images), np.array(perms)
+    
+    
 
 # Example main function for testing/development
 # Run this file using `python3 submission.py`
@@ -75,10 +129,21 @@ if __name__ == '__main__':
         predictor = Predictor()
         prediction = predictor.make_prediction(img_name)
         # Example images are all shuffled in the "3120" order
-        print(prediction)
 
         # Visualize the image
         pieces = utils.get_uniform_rectangular_split(np.asarray(example_image), 2, 2)
         # Example images are all shuffled in the "3120" order
-        final_image = Image.fromarray(np.vstack((np.hstack((pieces[3],pieces[1])),np.hstack((pieces[2],pieces[0])))))
-        final_image.show()
+
+
+
+        """
+        key = [1,2,3,4]
+        #save images
+        for index, num in enumerate(permutations([0,1,2,3])):
+            final_image = Image.fromarray(np.vstack((np.hstack((pieces[key[num[0]]],pieces[key[num[1]]])),np.hstack((pieces[key[num[2]]],pieces[key[num[3]]])))))
+            tempStr = str(num[0]) + str(num[1]) + str(num[2]) + str(num[3])
+            final_image.save(tempStr+".png")
+        """
+
+        final_image = Image.fromarray(np.vstack((np.hstack((pieces[int(prediction[0])],pieces[int(prediction[1])])),np.hstack((pieces[int(prediction[2])],pieces[int(prediction[3])])))))
+        final_image.save(prediction + " from " + img_name[15:])
