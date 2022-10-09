16e5b8c9df688eacac53732d79c11d8c6982d053
xProioxis <christionb@hotmail.com>
2022-10-08 21:31:34 -0500

    Base CNN Model

--- Challenges/TamuDatathon/TamuDatathon SourceCode/src/sequential_ex.py
+++ Challenges/TamuDatathon/TamuDatathon SourceCode/src/sequential_ex.py
@@ -9,17 +9,41 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Dense(2)) # will only have 2 outputs, scrambled or not scrambled
+model.add(layers.Flatten())
+model.add(layers.Dense(32, activation = "relu"))
+model.add(layers.Dense(1, activation = "sigmoid")) # will only have 2 outputs, scrambled or not scrambled
+
 
 model.compile(
   optimizer='adam',
-  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  loss="binary_crossentropy",
   metrics=['accuracy'])
 
 
 test_imgs = np.load('data0123.npy')
-print(test_imgs[0])
+test_imgs = np.squeeze(test_imgs, axis = 1)
+
+test_imgs_scrambled = np.load('data3210.npy')
+test_imgs_scrambled = np.squeeze(test_imgs_scrambled, axis = 1)
+
+totalImgs = np.concatenate((test_imgs, test_imgs_scrambled), axis=0)
+
+test_labels = np.zeros(np.shape(test_imgs)[0])
 
 
-print(model.predict(test_imgs[0]))
+print(np.shape(totalImgs))
+print(np.shape(test_labels))
+
+
+model.fit(
+  test_imgs,
+  test_labels,
+  epochs = 1
+)
+
+
+testInp = np.expand_dims(test_imgs[0],axis=0)
+print(np.shape(test_imgs[0]))
+print(np.shape(testInp))
+print(model.predict(testInp))
 
